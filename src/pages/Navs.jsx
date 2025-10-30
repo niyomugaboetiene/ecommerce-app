3883db3781b87edcc58fa80b140f97cf8fa81ff2
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-30 17:07:46 +0200

    first commit of ecommerce app

---
+++ src/pages/Navs.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+const Navs = () => {
+    return (
+        <div>
+             <Link>Home</Link>
+             <Link>Contact</Link>
+             <Link>About</Link>
+             <Link>Sign Up</Link>
+        </div> 
+    )
+}
