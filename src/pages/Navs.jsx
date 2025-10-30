491d964c12a05e4fcdf5d06c2bd150be6fccf9a1
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-30 17:49:12 +0200

    first commit of ecommerce app

--- src/pages/Navs.jsx
+++ src/pages/Navs.jsx
@@ -1,20 +1,25 @@
 import { Link } from "react-router-dom"
-import { FaSearch,  } from "react-icons/fa"
+import { FaSearch, FaHeart, FaShoppingCart } from "react-icons/fa"
 const Navs = () => {
     return (
-        <div>
-            <div>
+        <div className="shadow-2xl">
+            <p className="ms-3 text-2xl text-black font-bold">Shop Sphere</p>
+            <div className="flex space-x-9 font-normal justify-center items-center">
                 <Link to="/">Home</Link>
                 <Link to="/contact">Contact</Link>
                 <Link to="/about">About</Link>
                 <Link to="sign-Up">Sign Up</Link>
             </div>
-
-            <div>
-                <FaSearch />
-                <input type="text" name="What are you looking for" />
+           <div className="flex justify-end">
+            <div className="bg-gray-300 flex p-2 justify-center items-center mb-[50px] rounded-lg w-[300px]">
+                <input type="text" name="What are you looking for" className="focus:overflow-hidden border-none"/>
+                <FaSearch className="text-gray-700"/>
             </div>
-
+           <div className="flex">
+              <FaHeart className="bg-white"/>
+              <FaShoppingCart />
+           </div>
+           </div>
         </div> 
     )
 }
