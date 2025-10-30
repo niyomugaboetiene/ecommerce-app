e2b12df797528a6b0cc396128b14ee915ee22f3b
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-30 17:59:12 +0200

    first commit of ecommerce app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import phone1 from "../assets/electrics/phone1.jpg"
+import camera1 from "../assets/electrics/camera1.jpg"
+import pc1 from "../assets/electrics/pc.jpg"
 const Home = () => {
    return (
     <div>
-        
+
     </div>
    )
 }
