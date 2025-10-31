4d6d51277447a735b49ff8a47a9b2365dbeafbde
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-31 10:32:51 +0200

    commit of ecommerce app

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ import pc1 from "../assets/electrics/pc.jpg"
 import women1 from "../assets/clothes/women1.jpg"
 
 const Images = [
-   {image: phone1, description: ""}
+   {image: phone1, description: "Modern phones for better price"},
+   {image: camera1, description: "Modern camera Canon for better price"},
+   {image: pc1, description: "Modern laptop for better price"},
 ]
 const Home = () => {
    return (
