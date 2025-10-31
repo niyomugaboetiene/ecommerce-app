750a2933baad8a9bb6e45cdff8bdd158cf59e05f
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-10-31 16:45:00 +0200

    first commit of ecommerce app

--- src/pages/FlashSales.jsx
+++ src/pages/FlashSales.jsx
@@ -3,6 +3,27 @@ import  bag1 from "../assets/clothes/bag1.jpeg"
 import bag2 from "../assets/clothes/bag2.jpeg"
 import clothes1 from "../assets/clothes/clothes1.jpeg";
 import clothes2 from "../assets/clothes/clothes2.jpeg"
+import clothes3 from "../assets/clothes/clothes2.jpg"
+import clothes4 from "../assets/clothes/clothes3.jpeg"
+import clothes5 from "../assets/clothes/clothes4.jpeg"
+import clothes6 from "../assets/clothes/clothes5.jpeg"
+import clothes7 from "../assets/clothes/clothes7.jpeg"
+import clothes8 from "../assets/clothes/clothes8.jpeg"
+import camera1 from "../assets/electrics/camera1.jpeg"
+import camera2 from "../assets/electrics/camera2.jpeg"
+import camera3 from "../assets/electrics/camera3.jpeg"
+import camera4 from "../assets/electrics/camera4.jpeg"
+import car1 from "../assets/electrics/car1.jpg"
+
+const Images = [
+    {name: pent1, description: "This pent can be yours" },
+    {name: pent1, description: "This pent can be yours" },
+    {name: bag1, description: "This pent can be yours" },
+    {name: pent1, description: "This pent can be yours" },
+    {name: pent1, description: "This pent can be yours" },
+    {name: pent1, description: "This pent can be yours" }
+]
+
 const FlashSales = () => {
     return (
         <div>
