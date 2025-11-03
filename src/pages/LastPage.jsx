587f31db0a02d7b3e912b7259fb1254f92a4ecfe
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-03 14:18:45 +0200

    ecommerce: building about page

---
+++ src/pages/LastPage.jsx
@@ -0,0 +1,15 @@
+import ecommerce from "../assets/clothes/ecommerce-big_Big.webp"
+const LastPage =  () => {
+    return (
+        <div>
+            <div>
+                <aside>
+                    <img src={ecommerce} />
+                    <p>
+                        Believe us by clicking to add to cart button.
+                    </p>
+                </aside>
+            </div>
+        </div>
+    )
+}
