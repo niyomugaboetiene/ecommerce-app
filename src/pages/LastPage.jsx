cc3c8478a4e51915a08d4689b10b2a7eb4d450f6
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-03 14:30:33 +0200

    ecommerce: building about page

--- src/pages/LastPage.jsx
+++ src/pages/LastPage.jsx
@@ -2,7 +2,7 @@ import ecommerce from "../assets/speakers/speaker2.png";
 
 const LastPage =  () => {
     return (
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center bg-black">
             <div className="max-w-7xl relative group">
                     <img src={ecommerce} className="w-[800px] object-cover rounded-lg transition-transform duration-300"/>
                     <div className="absolute top-0 bottom-0 left-0 right-0 bg-black/30 p-4 rounded-lg flex flex-col items-center justify-end">
