7f1bebfc8119222445f23b0ceca50138bfea59f8
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-03 14:56:22 +0200

    ecommerce: building about footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -2,33 +2,45 @@ import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaTwitch, FaWhatsapp, Fa
 const Footer = () => {
     return (
         <div>
-            <div className="bg-black text-white">
-                  <h1>Shop Sphere </h1>
-                  <p>Subscribe</p>
-                  <input type="email" />
+            <div className="bg-black text-white grid grid-cols-5">
+                <div>
+                     <h1>Shop Sphere </h1>
+                     <p>Subscribe</p>
+                     <input type="email" />
+                </div>
 
-                  <h1>Support</h1>
-                  <p>RwandaMart in Rwanda</p>
-                  <p>+250 728 184 299</p>
-
-                  <h1>Account</h1>
-                  <p>My Account</p>
-                  <p>Login/ Register</p>
-                  <p>Cart</p>
-
+                 <div>
+                      <h1>Support</h1>
+                      <p>RwandaMart in Rwanda</p>
+                      <p>+250 728 184 299</p>
+                 </div>
+                <div>
+                    <h1>Account</h1>
+                    <p>My Account</p>
+                    <p>Login/ Register</p>
+                    <p>Cart</p>
+                </div>
+                 <div>
                   <h1>Quick Link</h1>
                   <p>Privancy Policy</p>
                   <p>Terms of use</p>
                   <p>Contact</p>
+                 </div>
 
+                   <div>
                   <h1>Let's connect</h1>
-                  <FaFacebook />
-                  <FaInstagram />
-                  <FaYoutube />
+                  <div>
+                <button> <FaFacebook /> </button>
+                <button><FaInstagram /> </button>
+                <button><FaYoutube /> </button>
                   <FaTwitter />
                   <FaTwitch />
                   <FaWhatsapp />
                   <FaLinkedinIn />
+                  </div>
+
+                   </div>
+
 
 
             </div>
