9e8a86ec5e2c12a4b70b43456afa8e924579178e
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-03 19:26:25 +0200

    backend: adding backend

--- backend/model.js
+++ backend/model.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose"
 
-mongoose.Schema({
-    
+
+const schema = new mongoose.schema({
+    product_id: {type: Number, unique: true, required: true },
+    product_name: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Float16Array, required: true },
+    likes: { type: Number },
+    date: { type: Date, default: Date.now() },
+    image: { type: String },
+    stock: { type: Number, default: 0 }
+
 })
