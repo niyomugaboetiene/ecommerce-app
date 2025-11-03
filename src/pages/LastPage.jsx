05f6638dc3bc9a57cea6cb24204272a4a1bc9376
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-03 14:35:40 +0200

    ecommerce: building about page

--- src/pages/LastPage.jsx
+++ src/pages/LastPage.jsx
@@ -3,12 +3,12 @@ import ecommerce from "../assets/speakers/speaker2.png";
 const LastPage =  () => {
     return (
         <div className="flex justify-center items-center">
-            <div className="max-w-7xl relative group  bg-black grid grid-cols-3">
+            <div className="max-w-7xl bg-black grid grid-cols-3">
                     <img src={ecommerce} 
                         className="w-[400px] object-cover rounded-lg transition-transform duration-300"
                     />
                         <p className="text-2xl text-white font-bold text-center mt-16">
-                           Believe us by clicking to add to cart button.
+                           Believe us by clicking to add to Buy Now button.
                          </p>
 
             </div>
