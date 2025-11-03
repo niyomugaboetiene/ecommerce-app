d2d05288e994b6b1b1edeb2203b31732f886baa3
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-03 20:14:10 +0200

    enhancing

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -6,8 +6,17 @@ const AddProduct = () => {
     const [category, setCategory] = useState("");
     const [price, setPrice] = useState("");
     const [stock, setStock] = useState("");
-    
+    const [isLoaing, setIsLoading] = useState(false);
+
     const Add = async() => {
-        
-    }   
+        try {
+            const res = await axios.post('http://localhost:5000/product/add', {
+                productName, category, price, stock
+            }, {
+                withCredentials: true
+            })
+        } catch (error) {
+            console.error("ERROR:", error.message);
+        }
+    }  
 }
