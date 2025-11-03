f9b5685df2880b1392995180594da32e68046ab8
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-03 19:47:02 +0200

    backend: adding backend

--- backend/ProductRoute.js
+++ backend/ProductRoute.js
@@ -1,3 +1,12 @@
 import productSchema from "./productSchema";
 import express from "express"
 const route = express.Router();
+
+route.post('/add', async(req, res) => {
+    try {
+        const product = await productSchema.create(req.body);
+        req.statusCode(201).json({
+            message: 'Product Added successfully'
+        })
+    }
+})
