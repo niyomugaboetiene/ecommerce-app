ccdf07201826be410acd558f82a4d015bb75f406
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-03 18:29:27 +0200

    ecommerce: building about footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,44 +9,65 @@ import LastPage from "./pages/LastPage"
 import Footer from "./pages/Footer"
 import OurProduct from "./pages/AllOurProduct"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-function App() {
 
+function App() {
   return (
     <>
       <BrowserRouter>
           <Navs />
-          <div className="pt-32 h-screen flex flex-col">
+          <div className="pt-32 min-h-screen flex flex-col">
            <Routes>
               <Route path="/" element={
-                <div className="overflow-y-scroll scroll-smooth">
-                      <section className="h-screen">
-                         <Home />
-                      </section>
-                    
+                <div className="min-h-screen flex flex-col">
+                  <div className="flex-grow overflow-y-scroll scroll-smooth">
                     <section>
-                         <FlashSales />
-                      </section>  
-
+                      <Home />
+                    </section>
                     <section>
-                         <BestSeller />
-                      </section>  
-                      <section>
-                         <OurProduct />
-                      </section>    
-                       <section>
-                         <LastPage />
-                      </section>
+                      <FlashSales />
+                    </section>  
+                    <section>
+                      <BestSeller />
+                    </section>  
+                    <section>
+                      <OurProduct />
+                    </section>    
+                    <section>
+                      <LastPage />
+                    </section>
+                  </div>
+                  <Footer />
                 </div>
-              }
-             />
+              }/>
 
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/footer" element={<Footer />} />
-         </Routes>
+              <Route path="/contact" element={
+                <div className="min-h-screen flex flex-col">
+                  <div className="flex-grow">
+                    <Contact />
+                  </div>
+                  <Footer />
+                </div>
+              } />
+              
+              <Route path="/about" element={
+                <div className="min-h-screen flex flex-col">
+                  <div className="flex-grow">
+                    <About />
+                  </div>
+                  <Footer />
+                </div>
+              } />
+              
+              <Route path="/sign-up" element={
+                <div className="min-h-screen flex flex-col">
+                  <div className="flex-grow">
+                    <SignUp />
+                  </div>
+                  <Footer />
+                </div>
+              } />
+           </Routes>
           </div>
-
       </BrowserRouter>
     </>
   )
