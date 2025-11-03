9bc005da55a2b9b2efbbf725ad38bd2a54bb49cc
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-03 19:36:57 +0200

    backend: adding backend

--- backend/productSchema.js
+++ backend/productSchema.js
@@ -5,7 +5,7 @@ const ProductSchema = new mongoose.schema({
     product_id: {type: Number, unique: true, required: true },
     product_name: { type: String, required: true },
     category: { type: String, required: true },
-    price: { type: Float16Array, required: true },
+    price: { type: Number, required: true },
     likes: { type: Number },
     date: { type: Date, default: Date.now() },
     image: { type: String },
--- backend/userSchema.js
+++ backend/userSchema.js
@@ -1,15 +1,13 @@
+import mongoose from "mongoose";
 import mongoose from "mongoose"
 
 
-const ProductSchema = new mongoose.schema({
-    product_id: {type: Number, unique: true, required: true },
-    product_name: { type: String, required: true },
-    category: { type: String, required: true },
-    price: { type: Float16Array, required: true },
-    likes: { type: Number },
-    date: { type: Date, default: Date.now() },
+const UserSchema = new mongoose.schema({
+    user_id: {type: Number, unique: true, required: true },
+    user_name: { type: String, required: true },
+    password: { type: Number, required: true },
     image: { type: String },
-    stock: { type: Number, default: 0 }
+    isAdmin: { type: Boolean, default: false }
 
 });
 
