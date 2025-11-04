c7ddcba527fa8188a743caafe317988f76c01a23
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-04 17:39:41 +0200

    implementation about user

--- src/pages/Health.jsx
+++ src/pages/Health.jsx
@@ -46,11 +46,11 @@ const Health = () => {
                         >
                               <img src={`http://localhost:5000/${item.image}`} className="w-full h-full object-cover hover:scale-105 transition duration-200"
                               />
-                                             {isHoveredIndex === idx && (
-                                                              <button className="absolute top-2 right-2 bg-white p-2 rounded-full shadow hover:bg-red-100 transition">
-                                                                  <FaHeart className="text-red-500" />
-                                                              </button>
-                                                          )}
+                             {isHoveredIndex === idx && (
+                                  <button className="absolute top-2 right-2 bg-white p-2 rounded-full shadow hover:bg-red-100 transition">
+                                         <FaHeart className="text-red-500" />
+                                 </button>
+                             )}
                             </div>
          
                           <p className="text-[15px] text-gray-600">Name: {item.product_name}</p>
