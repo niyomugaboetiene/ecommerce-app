db18c32d6683d0ea00e4c036ed1d76968db57de6
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-04 08:52:47 +0200

    implementing about product routes

--- src/pages/BestSeller.jsx
+++ src/pages/BestSeller.jsx
@@ -1,47 +1,30 @@
-import speaker1 from "../assets/speakers/speaker1.jpg";
-import speaker2 from "../assets/speakers/speaker2.jpeg";
-import speaker3 from "../assets/speakers/speaker3.jpeg";
-import speaker4 from "../assets/speakers/speaker4.jpeg";
-import speaker5 from "../assets/speakers/speaker5.jpeg";
-import speaker6 from "../assets/speakers/speaker6.jpeg";
-import speaker7 from "../assets/speakers/speaker7.jpeg";
-import speaker9 from "../assets/speakers/speaker9.jpg";
-import camera1 from "../assets/electrics/camera1.jpeg"
-import electcar from "../assets/electrics/electcar.jpg"
-import homeDesk from "../assets/electrics/homeDesk.jpeg"
-import iphone from "../assets/electrics/iphone.jpg"
-import joystick from "../assets/electrics/joystick.jpg"
-import joystick1 from "../assets/electrics/joystick3.jpg"
-import joystick2 from "../assets/electrics/joystick2.jpg"
+
 import { FaShoppingCart } from "react-icons/fa";
 import joystick3 from "../assets/electrics/joystick3.webp"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 
-const Images = [
-    {image: speaker1, decription: "speaker"},
-    {image: speaker2, decription: "speaker"},
-    {image: speaker3, decription: "speaker"},
-    {image: speaker4, decription: "speaker"},
-    {image: speaker5, decription: "speaker"},
-    {image: speaker6, decription: "speaker"},
-    {image: speaker7, decription: "speaker"},
-    {image: speaker9, decription: "speaker"},
-    {image: camera1, decription: "speaker"},
-    {image: electcar, decription: "speaker"},
-    {image: homeDesk, decription: "speaker"},
-    {image: iphone, decription: "speaker"},
-    {image: joystick, decription: "speaker"},
-    {image: joystick1, decription: "speaker"},
-    {image: joystick2, decription: "speaker"},
-    {image: joystick3, decription: "speaker"}
-];
 
 const BestSeller = () => {
     const [showAll, setShowAll] = useState(false);
+    const [products, setProducts] = useState([]);
     const [isHoveredIndex, setIsHoveredIndex] = useState(null);
 
-    const imageToSHow = showAll ? Images : Images.slice(0, 8);
+        useEffect(() => {
+            try {
+                 const GetBestSold = async() => {
+                      const product = await axios.get('http://localhost:5000/product/best-sold', {withCredentials: true});
+                      setProducts(product.data.products);
+                };
+                
+             GetBestSold();
+            } catch (error) {
+                console.log("ERROR: ", error.message);
+            }
+        }, []);
+
+    const imageToSHow = showAll ? product : Images.slice(0, 8);
     const hasMoreItems = Images.length > 8;
     
     return (
