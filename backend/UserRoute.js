4f1114fc20cb70e8612bf159385836ac5a5a9cb7
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-04 15:05:15 +0200

    implementation about user

--- backend/UserRoute.js
+++ backend/UserRoute.js
@@ -16,13 +16,18 @@ const storage = multer.diskStorage({
 const uploads = multer({ storage });
 
 routes.post('/register', uploads.single('image'), async(req, res) => {
+    try {
     const { user_name, password } = req.body;
     const ImagePath = req.file ? req.file.path : null;
-
-    try {
+    
+    if (!user_name || !password || ImagePath) {
+        res.status(400).json({ message: 'Missing dependecies' });
+    }
         await UserSchema.create({
             user_name, password, image: ImagePath
         });
+        
+        return res.status(201).json({ message: 'User registered successfully' });
     } catch(error) {
          res.status(500).json({message: 'Database error'});
     }      
