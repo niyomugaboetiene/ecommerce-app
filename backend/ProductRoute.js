9121593ce82ef6108f337794d35a057f0e1344e8
niyomugaboetiene <niyomugaboetiene53@gmail.com>
2025-11-04 07:23:52 +0200

    implementing about product routes

--- backend/ProductRoute.js
+++ backend/ProductRoute.js
@@ -1,7 +1,16 @@
 import ProductSchema from "./productSchema.js";
 import express from "express"
+import multer from "multer";
 const route = express.Router();
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'Product_Image/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname); // * to avoid duplicates
+    }
+})
 route.post('/add', async(req, res) => {
     try {
         await ProductSchema.create(req.body);
